beab9ba27cb69b4478906868665f0c7566f924f1
George van Vliet <george@georgevanvliet.nl>
2019-12-05 20:14:01 +0100

    Add oauth implicit flow to swagger

--- src/Controller/RootController.php
+++ src/Controller/RootController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Twig\Environment;
 
 /**
  * Class RootController
@@ -21,14 +22,20 @@ class RootController
      * @var EntityManagerInterface
      */
     private $em;
+    /**
+     * @var Environment
+     */
+    private $twig;
 
     /**
      * RootController constructor.
      * @param EntityManagerInterface $em
+     * @param Environment $twig
      */
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, Environment $twig)
     {
         $this->em = $em;
+        $this->twig = $twig;
     }
 
     /**
@@ -44,5 +51,13 @@ class RootController
             $content = 'No user id available.';
         }
         return new Response($content);
+    }
+
+    /**
+     * @Route("/oauth2-redirect.html", methods={"GET"})
+     */
+    public function swaggerUiOAuth2Redirect()
+    {
+        return new Response($this->twig->render('oauth2-redirect.html.twig'));
     }
 }
